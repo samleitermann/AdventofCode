3a52757fbb437846933e97b734078bd821fc082a
Lightermann <sam.leitermann@gmail.com>
2019-12-08 21:18:13 -0500

    Changed data input, updated code after revisions.

--- AdventofCode2019/Day 7/Day7.py
+++ AdventofCode2019/Day 7/Day7.py
@@ -5,14 +5,14 @@ def get_data(file):
         data = list(map(int, f.read().split(",")))
     return data
 
-data = get_data("Day7Input.txt")
+data = get_data("/Users/sleitermann-long/Documents/AdventofCode/AdventofCode2019/Day 7/Day7Input.txt")
 
 pointerjump = {1: 4, 2: 4, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4, 99: 0}
 
 def opcodecomp(instrs,UserInput,pointer):
 
   instr = list.copy(instrs)
-  position = 0
+  position = pointer
   j = 0
 
   while True:
@@ -20,6 +20,7 @@ def opcodecomp(instrs,UserInput,pointer):
     rawopcode = instr[position]
     opcode = rawopcode
     parameterMode = False #0 = position mode
+
     if rawopcode > 100:
       parameterMode = True #We now have to determine parameters
       opcode= int(str(rawopcode)[-2:]) #slices off the last 2 digits to tell us the operation
@@ -39,39 +40,57 @@ def opcodecomp(instrs,UserInput,pointer):
     if opcode == 1:
       #add
       instr[outputPos] = instr[input1Pos]+instr[input2Pos]
+      if not pointerChanged:
+        position = position + pointer
     if opcode == 2:
       #multiply
       instr[outputPos] = instr[input1Pos]*instr[input2Pos]
+      if not pointerChanged:
+        position = position + pointer
     if opcode == 3:
       #put input in position
       instr[instr[position+1]] = UserInput[j]
       j+=1
+      if not pointerChanged:
+        position = position + pointer
     if opcode == 4:
       #output
-      if instr[input1Pos] != 0:
-        return [instr[input1Pos], position, instr]
+      #if instr[input1Pos] != 0:
+      if not pointerChanged:
+        position = position + pointer
+      return [instr[input1Pos], position, instr, opcode]
     if opcode == 5:
       #jump if true
       if instr[input1Pos] != 0:
         pointerChanged = True
         position = instr[input2Pos]
+        if not pointerChanged:
+          position = position + pointer
     if opcode == 6:
       #jump if false
       if instr[input1Pos] == 0:
         pointerChanged = True
         position = instr[input2Pos]
+        if not pointerChanged:
+          position = position + pointer
     if opcode == 7:
       #less than
       instr[outputPos] = 1 if instr[input1Pos]<instr[input2Pos] else 0
+      if not pointerChanged:
+        position = position + pointer
     if opcode == 8:
       #equals
       instr[outputPos] = 1 if instr[input1Pos]==instr[input2Pos] else 0
+      if not pointerChanged:
+        position = position + pointer
     if opcode == 99:
       #halt
-      break
+      if not pointerChanged:
+        position = position + pointer
+      return [None, position, instr, opcode]
 
-    if not pointerChanged:
-      position = position + pointer
+    #if not pointerChanged:
+      #position = position + pointer
 
 def amplifier(instrs, ctrlsequence, signal = 0, stage = 0):
   #define how many stages there are
@@ -82,24 +101,7 @@ def amplifier(instrs, ctrlsequence, signal = 0, stage = 0):
   signal = opcodecomp(data, [int(ctrlsequence[stage]), signal],0)[0]
   return amplifier(instrs, ctrlsequence, signal, stage + 1)
 
-sequences1 = list(permutations("43210"))
-sequences2 = list(permutations("98765"))
-
-ans_1 = []
-for seq in sequences1:
-  ans_1.append(amplifier(data, seq))
-
-print('Part 1: ', max(ans_1))
-
-
-#print("part 1: ", max(ans_1))
-
-
-
-def amplify_feedback(stages, instrs, ctrlsequence, pointers, signal=0, stage=0, last_result=0):
-
-  if isinstance(signal, list):
-      return last_result
+def amplify_feedback(stages, program, ctrlsequence, pointers, signal=0, stage=0, last_result=0, currentopcode = 0):
 
   if stage == stages:
       last_result = signal
@@ -111,20 +113,30 @@ def amplify_feedback(stages, instrs, ctrlsequence, pointers, signal=0, stage=0, 
   else:
       call = [signal]
 
-  signal, pointers[stage], instrs[stage] = opcodecomp(
-      instrs[stage], call, pointers[stage])
+  signal, pointers[stage], program[stage], currentopcode = opcodecomp(program[stage], call, pointers[stage])
 
-  return amplify_feedback(
-      stages, instrs, ctrlsequence, pointers, signal, stage + 1, last_result)
+  if currentopcode == 99:
+    return last_result
 
-results = set()
+  else:
+    return amplify_feedback(stages, program, ctrlsequence, pointers, signal, stage + 1, last_result)
 
+sequences1 = list(permutations("43210"))
+sequences2 = list(permutations("98765"))
+
+ans_1 = []
+for seq in sequences1:
+  ans_1.append(amplifier(data, seq))
+
+print('Part 1: ', max(ans_1))
+
+ans_2 = set()
 
 for seq in sequences2:
     # seq = [9, 7, 8, 5, 6]
     pointers = [0, 0, 0, 0, 0]
-    instrs = [data for i in range(5)]
+    programs = [data for i in range(5)]
+    ans_2.add(amplify_feedback(5, programs, seq, pointers))
 
-    results.add(amplify_feedback(5, instrs, seq, pointers))
 
-print(f"PART 2: {max(results)}")
+print("Part 2: ", max(ans_2))
