380e6e0e54a9070106e11e89d1a6b233c6e47708
samleitermann <sam.leitermann@gmail.com>
2020-12-03 09:47:29 -0500

    Actually fixed the indenting this time?

--- AdventofCode2020/Day3/Day3Solution.py
+++ AdventofCode2020/Day3/Day3Solution.py
@@ -11,54 +11,54 @@ directions = [[1, 1, 0, 1, 0],[3, 1, 0, 1, 0],[5, 1, 0, 1, 0],[7, 1, 0, 1, 0],[1
 #part one
 
 def part_one(in_terrain, map_length, direction):
-  #set the parameters
-  right = direction[0]
-  down = direction[1]
-  position = direction[2]
+    #set the parameters
+    right = direction[0]
+    down = direction[1]
+    position = direction[2]
 
-  #need to count lines to skip lines when our slope becomes non-integer
-  line_count = 0
+    #need to count lines to skip lines when our slope becomes non-integer
+    line_count = 0
 
-  #loop through terrain and then (1) check if we're on an appropriate line for the slope, (2) check if we've hit a tree, and (3) check our position and loop back in the line if necessary.
-  for terr in in_terrain:
-    #check that we've moved down far enough.
-    if line_count % down == 0:
-      #check if we've hit a tree
-      if terr[position] == '#':
-        direction[4] += 1
-
-
-      #if we won't hit the end of the line, great!
-      if (position+right) < (map_length - 1):
-        position += right
-      #if we do hit the end of the line, loop us back to the beginning and restart.
-      else:
-        position = position - map_length + right
-    #note that we have moved down a line
-    line_count += 1
+    #loop through terrain and then (1) check if we're on an appropriate line for the slope, (2) check if we've hit a tree, and (3) check our position and loop back in the line if necessary.
+    for terr in in_terrain:
+        #check that we've moved down far enough.
+        if line_count % down == 0:
+            #check if we've hit a tree
+            if terr[position] == '#':
+                direction[4] += 1
 
 
-  return(direction[4])
+        #if we won't hit the end of the line, great!
+        if (position+right) < (map_length - 1):
+            position += right
+            #if we do hit the end of the line, loop us back to the beginning and restart.
+        else:
+            position = position - map_length + right
+            #note that we have moved down a line
+        line_count += 1
+
+
+        return(direction[4])
 
 #solution to part one (Moved to bottom to not be dumb)
 #print("Part One:", part_one(terrain, len(terrain[0]), directions[1]))
 
 def part_two(in_terrain,directions):
-  #count the number of times to run part_one and add a product variable to calculate the product
-  count = 0
-  product = 1
+    #count the number of times to run part_one and add a product variable to calculate the product
+    count = 0
+    product = 1
 
-  temp_directions = directions
+    temp_directions = directions
 
-  #loop through part one with different parameters
-  for direction in temp_directions:
-    part_one(in_terrain,len(in_terrain[0]),temp_directions[count])
-    count +=1
+    #loop through part one with different parameters
+    for direction in temp_directions:
+        part_one(in_terrain,len(in_terrain[0]),temp_directions[count])
+        count +=1
 
-  for i in range(count):
-    product = product*temp_directions[i][4]
+    for i in range(count):
+        product = product*temp_directions[i][4]
 
-  return(temp_directions[1][4],product)
+    return(temp_directions[1][4],product)
 
 print("Answers:", part_two(terrain,directions))
 
