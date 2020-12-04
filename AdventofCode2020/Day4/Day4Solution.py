a048ac7b27004480e2cef43f8f104b588cc2707e
samleitermann <sam.leitermann@gmail.com>
2020-12-04 14:42:00 -0500

    Added the input.

---
+++ AdventofCode2020/Day4/Day4Solution.py
@@ -0,0 +1,36 @@
+# Open data and strip out newlines
+
+def get_data(file):
+    with open(file, encoding='utf-8') as f:
+        data = f.read()
+        inputs = data.split('\n\n')
+    return inputs
+
+
+# fetch data and initialize passport array, define fields and
+passport_input = get_data("Day4Input.txt")
+
+passports = []
+
+fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid']
+fields.remove('cid')
+
+part_one = 0
+
+
+# turn the passports into a dictionary
+
+for index, i in enumerate(passport_input):
+    rawdata = i.replace('\n', ' ').split()
+    details = [i.split(':') for i in rawdata]
+    passport_dict = {x[0]: x[1] for x in details}
+    passports.append(passport_dict)
+
+# count valid passports
+
+for index, passport in enumerate(passports):
+    keys = passport.keys()
+    if all(field in keys for field in fields):
+        part_one += 1
+
+print("Part One:", part_one)
