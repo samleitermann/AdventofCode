9d215a1c488c49299b51f3c9f68c04407e843b90
Sam Leitermann-Long <sam.leitermann@gmail.com>
2021-12-03 03:39:49 +0000

    Final Solution for Day 1

--- AdventofCode2021/Day1.py
+++ AdventofCode2021/Day1.py
@@ -5,15 +5,31 @@ def get_data(file):
 
 def part1(depths):
   count = 0
-  measurements = len(depths)=1
-  while i< measurements:
-    if depth[i] > depth[i+1]:
+  measurements = len(depths)
+  i=0
+  while i < measurements-1:
+    if depths[i] < depths[i+1]:
       count +=1
     i+=1
 
   return count
 
+def part2(depths):
+  count = 0
+  i=0
+  measurements = len(depths)
+  while i< measurements - 3:
+    depth1 = depths[i]+depths[i+1]+depths[i+2]
+    depth2 = depths[i+1]+depths[i+2]+depths[i+3]
+    if depth2 >depth1:
+      count+=1
+    i+=1
+  
+  return count
+
 depthmeasurements = get_data("Day1Data.txt")
 
-print(depthmeasurements)
+print(part1(depthmeasurements))
+print(part2(depthmeasurements))
+
 
