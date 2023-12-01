098bd822aac96a7d11e44c747b15d5e61406d930
Sam Leitermann-Long <sam.leitermann@gmail.com>
2023-12-01 14:28:14 -0500

    Day 7 Solution - 2022

---
+++ AdventofCode2022/Day7/Day7.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+with open("Day7Data.txt") as f:
+    commands = f.readlines()
+
+sizes = defaultdict(int)
+stack = []
+
+for c in commands:
+    if c.startswith("$ ls") or c.startswith("dir"):
+        continue
+    if c.startswith("$ cd"):
+        dest = c.split()[2]
+        if dest == "..":
+            stack.pop()
+        else:
+            path = f"{stack[-1]}_{dest}" if stack else dest
+            stack.append(path)
+    else:
+        size, file = c.split()
+        for path in stack:
+            sizes[path] += int(size)
+
+needed_size = 30000000 - (70000000 - sizes['/'])
+for size in sorted(sizes.values()):
+    if size > needed_size:
+        break
+
+
+print(sum(n for n in sizes.values() if n<=100000))
+print(size)
