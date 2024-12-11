80ef8bba27be00a109e0ea89645484bad9e36d54
Sam Leitermann-Long <sam.leitermann@gmail.com>
2024-12-10 20:16:16 -0500

    deleting files

--- problems/2024/day_1/solution.py
+++ problems/2024/day_1/solution.py
@@ -5,20 +5,15 @@ class Solution:
   filename_test_input = 'test_input.txt'
   
   def __init__(self, test=False):
-    self.file = open(self.filename_test_input,'r').read() if test else open(self.filename_real_input,'r').read()
-    self.lines = [l.split('  ') for l in self.file.splitlines()]
-    self.list1 = sorted([int(l[0]) for l in self.lines])
-    self.list2 = sorted([int(l[1]) for l in self.lines])
+    self.filename = self.filename_test_input if test else self.filename_real_input
+    self.file = open(self.filename,'r').read()
+    self.lines = self.file.splitlines()
     
   def part1(self):
-    return sum(abs(l-r) for l,r in zip(self.list1,self.list2))
+    pass
   
   def part2(self):
-    similarity_score = 0
-    for num_l in self.list1:
-      nb_occurs_r = len([num_r for num_r in self.list2 if num_r == num_l])
-      similarity_score += num_l * nb_occurs_r
-    return similarity_score
+    pass
   
 if __name__ == '__main__':
   parser = argparse.ArgumentParser('Solution file')
