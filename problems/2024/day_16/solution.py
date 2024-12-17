193f004507cdc508a22afea73bdf5eb07cf7922e
Sam Leitermann-Long <sam.leitermann@gmail.com>
2024-12-17 09:41:39 -0500

    Hacked together a NetworkX solution based on a Reddit comment.

--- problems/2024/day_16/solution.py
+++ problems/2024/day_16/solution.py
@@ -1,4 +1,5 @@
 import argparse
+import networkx as nx
 
 class Solution:
   filename_real_input = 'real_input.txt'
@@ -8,12 +9,76 @@ class Solution:
     self.filename = self.filename_test_input if test else self.filename_real_input
     self.file = open(self.filename,'r').read()
     self.lines = self.file.splitlines()
-    
+
+  def create_graph(self):
+    NR, NC = len(self.lines), len(self.lines[0])
+
+    board = set()
+    Graph = nx.Graph()
+    grid = self.lines
+
+    directions = ['^', '>', 'v', '<']
+
+    for r in range(NR):
+      for c in range(NC):
+        ch = grid[r][c]
+
+        if ch == 'S':
+          start = r,c,'>'
+        elif ch  == 'E':
+          end = r,c,'?'
+
+        if ch != '#':
+          board.add((r,c))
+
+    for r in range(NR):
+      for c in range (NC):
+        if (r,c) in board:
+          for dir in directions:
+            Graph.add_node((r,c,dir))
+          for i, direction in enumerate(directions):
+            Graph.add_edge((r,c,direction), (r,c,directions[(i+1)%4]), weight = 1000)
+
+          if (r - 1, c) in board:
+            Graph.add_edge((r, c, '^'), (r - 1, c, '^'), weight=1)
+
+          if (r + 1, c) in board:
+            Graph.add_edge((r, c, 'v'), (r + 1, c, 'v'), weight=1)
+
+          if (r, c - 1) in board:
+            Graph.add_edge((r, c, '<'), (r, c - 1, '<'), weight=1)
+
+          if (r, c + 1) in board:
+            Graph.add_edge((r, c, '>'), (r, c + 1, '>'), weight=1)
+
+    for dir in directions:
+      Graph.add_edge((end[0],end[1],dir),end,weight=0)
+
+    dist_from_start, _ = nx.single_source_dijkstra(Graph, source = start, weight = 'weight')
+    dist_from_end, _ = nx.single_source_dijkstra(Graph, source = end, weight = 'weight')
+
+    return dist_from_start, dist_from_end, start, end, Graph
+
   def part1(self):
-    pass
+    result =self.create_graph()
+    end = result[3]
+    dist_from_start = result[0]
+
+    min_cost = dist_from_start[end]
+
+    return min_cost
   
   def part2(self):
-    pass
+    result = self.create_graph()
+    G = result[4]
+    end = result[3]
+    dist_from_start = result[0]
+    dist_from_end = result[1]
+    min_cost = dist_from_start[end]
+
+    locs = { (r,c) for r,c, d in G.nodes() if min_cost ==(dist_from_start[r,c,d]+dist_from_end[r,c,d])}
+
+    return len(locs)
   
 if __name__ == '__main__':
   parser = argparse.ArgumentParser('Solution file')
