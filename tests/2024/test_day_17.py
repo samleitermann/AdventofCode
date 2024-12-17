15f14b25737c779b42d228cec5329c26b94ac711
Sam Leitermann-Long <sam.leitermann@gmail.com>
2024-12-17 08:51:35 -0500

    Added Day17 files

---
+++ tests/2024/test_day_17.py
@@ -0,0 +1,10 @@
+import subprocess
+expected_output = {'1':None, '2':None}
+day = '17'
+year = '2024'
+
+def test_solution():
+  for part in ['1','2']:
+    command = ['./submit', day, year, part, 'true']
+    result = subprocess.run(command, stdout=subprocess.PIPE, text=True, check=True)
+    assert result.stdout == f'Result for Part=={part} & Test==True : {expected_output[part]}\n'
